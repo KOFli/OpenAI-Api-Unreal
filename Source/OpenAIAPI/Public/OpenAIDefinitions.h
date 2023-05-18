5f4a6a26e3244b1c46f304bb8a2fdd15a5fdff3d
KOFli <48345483+KOFli@users.noreply.github.com>
2023-05-18 20:31:34 +0800

    Update OpenAIDefinitions.h

--- Source/OpenAIAPI/Public/OpenAIDefinitions.h
+++ Source/OpenAIAPI/Public/OpenAIDefinitions.h
@@ -29,6 +29,13 @@ enum class EOAImageSize : uint8
 	LARGE = 2 UMETA(ToolTip = "Generates 1024x1024 images. This setting takes the longest amount of time to generate images.")
 };
 
+UENUM(BlueprintType)
+enum class EOATurboModel : uint8
+{
+	TURBO = 0 UMETA(ToolTip = "Most capable GPT-3.5 model and optimized for chat at 1/10th the cost of . Will be updated with our latest model iteration.text-davinci-003"),
+	TURBO_0301 = 1 UMETA(ToolTip = "Snapshot of from March 1st 2023. Unlike , this model will not receive updates, and will only be supported for a three month period ending on June 1st 2023.gpt-3.5-turbogpt-3.5-turbo")
+};
+
 // Structs for GPT3
 USTRUCT(BlueprintType)
 struct FCompletionInfo
@@ -83,7 +90,23 @@ struct FCompletion
 
 };
 
+USTRUCT(BlueprintType)
+struct FChatCompletion
+{
+	GENERATED_BODY()
 
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "OpenAI")
+		int32 index = 0;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "OpenAI")
+		FString role = "";
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "OpenAI")
+		FString content = "";
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "OpenAI")
+		FString finishReason = "";
+};
 
 class OpenAIValueMapping
 {
@@ -92,10 +115,57 @@ public:
 	
 	TMap<EOAEngineType, FString> engineTypes;
 	TMap<EOAImageSize, FString> imageSizes;
+	TMap<EOATurboModel, FString> turboModels;
 };
 
 USTRUCT(BlueprintType)
-struct FGPT3Settings
+struct FOAChatMessage
+{
+	GENERATED_BODY()
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "OpenAI")
+	FString Role;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "OpenAI")
+	FString Content;
+};
+
+USTRUCT(BlueprintType)
+struct FOASettings
+{
+	GENERATED_BODY()
+
+	/** The maximum number of tokens to generate. Requests can use up to 2048 tokens shared between prompt and completion. (One token is roughly 4 characters for normal English text) */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "OpenAI")
+	int32 maxTokens = 16;
+
+	/** What sampling temperature to use. Higher values means the model will take more risks. Try 0.9 for more creative applications, and 0 (argmax sampling) for ones with a well-defined answer. */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "OpenAI")
+		float temperature = 1.0f;
+
+	/** An alternative to sampling with temperature, called nucleus sampling, where the model considers the results of the tokens with top_p probability mass. So 0.1 means only the tokens comprising the top 10% probability mass are considered.  */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "OpenAI")
+		float topP = 1.0f;
+
+	/** How many completions to generate for each prompt. */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "OpenAI")
+		int32 numCompletions = 1;
+
+	/** Number between 0 and 1 that penalizes new tokens based on their existing frequency in the text so far. Decreases the model's likelihood to repeat the same line verbatim. */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "OpenAI")
+		float frequencyPenalty = 0.0f;
+
+	/** Number between 0 and 1 that penalizes new tokens based on whether they appear in the text so far. Increases the model's likelihood to talk about new topics. */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "OpenAI")
+		float presencePenalty = 0.0f;
+
+	/** Up to four sequences where the API will stop generating further tokens. The returned text will not contain the stop sequence. */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "OpenAI")
+		TArray<FString> stopSequences;
+};
+
+USTRUCT(BlueprintType)
+struct FGPT3Settings :public FOASettings
 {
 	GENERATED_BODY()
 
@@ -111,40 +181,11 @@ struct FGPT3Settings
 	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "OpenAI")
 		FString injectRestartText = "";
 
-	/** Up to four sequences where the API will stop generating further tokens. The returned text will not contain the stop sequence. */
-	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "OpenAI")
-		TArray<FString> stopSequences;
-
-	/** The maximum number of tokens to generate. Requests can use up to 2048 tokens shared between prompt and completion. (One token is roughly 4 characters for normal English text) */
-	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "OpenAI")
-		int32 maxTokens = 16;
-
-	/** What sampling temperature to use. Higher values means the model will take more risks. Try 0.9 for more creative applications, and 0 (argmax sampling) for ones with a well-defined answer. */
-	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "OpenAI")
-		float temperature = 1.0f;
-
-	/** An alternative to sampling with temperature, called nucleus sampling, where the model considers the results of the tokens with top_p probability mass. So 0.1 means only the tokens comprising the top 10% probability mass are considered.  */
-	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "OpenAI")
-		float topP = 1.0f;
-
 	/** If logProbs is greater than 0, the model returns tokens, token_logprobs, and the text_offsets for a given completion. (support for top_logprobs will be added soon)*/
 	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "OpenAI")
 		int32 logprobs = 0;
 
-	/** How many completions to generate for each prompt. */
-	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "OpenAI")
-		int32 numCompletions = 1;
-
-	/** Number between 0 and 1 that penalizes new tokens based on their existing frequency in the text so far. Decreases the model's likelihood to repeat the same line verbatim. */
-	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "OpenAI")
-		float frequencyPenalty = 0.0f;
-
-	/** Number between 0 and 1 that penalizes new tokens based on whether they appear in the text so far. Increases the model's likelihood to talk about new topics. */
-	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "OpenAI")
-		float presencePenalty = 0.0f;
-
 	/** Generates best_of completions server-side and returns the "best" (the one with the lowest log probability per token). */
 	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "OpenAI")
 		int32 bestOf = 1;
-
 };
